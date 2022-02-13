882bdef1d2febe3a723ed77995bbb2c737398563
SandmanFrost <90185784+SandmanFrost@users.noreply.github.com>
2022-02-13 10:27:22 +0300

    Create Game.c

---
+++ Game.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <conio.h>
+#include <time.h>
+#include <locale.h>
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	char mas[10][21];
+	int i, n = 0;
+	int x = 10, y = 5;
+	char key;
+	int ox,oy;
+	int ax,ay;
+
+	srand(time(NULL));
+
+	ax = rand() * 1.0 / RAND_MAX * 18 + 1;
+	ay = rand() * 1.0 / RAND_MAX * 8 + 1;
+
+	do
+	{
+		sprintf(mas[0], "####################");
+		for (i = 1; i < 9; i++)
+			sprintf(mas[i], "#                  #");
+		sprintf(mas[9], "####################");
+
+		mas[y][x] = '@';
+		mas[ay][ax] = '*';
+
+		system("cls");
+		for (i = 0; i < 10; i++)
+			printf("%s\n", mas[i]);
+		printf("Score: %d \n", n);
+		printf("Help: \'W\',\'A\',\'S\',\'D\' - move, \'E\' - exit\n");
+		key = getch();
+
+		ox = x;
+		oy = y;
+		if (key == 'w') y--;
+		if (key == 's') y++;
+		if (key == 'a') x--;
+		if (key == 'd') x++;
+		if (mas[y][x] == '#')
+		{
+			x = ox;
+			y = oy;
+		}
+		if ((x == ax) && (y == ay))
+		{
+			ax = rand() * 1.0 / RAND_MAX * 18 + 1;
+			ay = rand() * 1.0 / RAND_MAX * 8 + 1;
+			n++;
+		}
+	}
+	while (key != 'e');
+	return 0;
+}
